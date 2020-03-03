c8b63ecb502aa27e60c5c279b73dd94deacdb670
14hat <14hattoj@gmail.com>
2020-03-03 13:06:49 +0000

    Added logger

--- Logging.py
+++ Logging.py
@@ -9,6 +9,7 @@ class Logging(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     async def on_message_delete(message):
         em = discord.Embed(title='Message Deleted', colour=0x0000ff)
         em.add_field(name='Member:', value=message.author, inline=False)
